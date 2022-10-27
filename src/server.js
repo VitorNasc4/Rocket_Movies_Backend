15cf11978a809ccdd8629730a9bfeb27c1c5953d
vitor <vitornascimento@321@hotmail.com>
2022-10-27 14:10:51 -0300

    fix .env variable

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ const uploadConfig = require("./configs/upload")
 
 const cors = require("cors")
 
-const PORT = process.env.SERVER_PORT || 3333
+const PORT = process.env.PORT || 3333
 app.use(cors())
 app.use(express.json())
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
